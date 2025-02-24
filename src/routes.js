632e035b557bf908ba731eb5c590e420442b9d93
jrlk <joaopbnascimento1@gmail.com>
2025-02-24 15:00:08 -0300

    Foi criado a parte de Login do site, com todos os campos necessários. Completamente funcional. Foram usados o Axios, criados os Cards e FromGroups e o Navigate.

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import LandingPage from './pages/LandingPage';
+import Login from "./pages/Login";
 
 export default function AppRoutes() {
     return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
     );
